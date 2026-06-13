87faae78f65ab1286b460994c34b21305c73441e
Jurica Separovic <jurica.separovic@gmail.com>
2026-06-13 20:43:44 +0200

    Add Tomic consolation win over Martinovic

--- website/schedule-data.js
+++ website/schedule-data.js
@@ -39,7 +39,7 @@
       { start: "16:20", end: "16:40", phase: "zavrsnica", stage: "1/4", bracket: "glavni", format: "6", sides: [{ player: "jurica-separovic", score: 6 }, { player: "ian-miocic", score: 3 }] },
       { start: "16:40", end: "17:00", phase: "zavrsnica", stage: "1/4", bracket: "glavni", format: "6", sides: [{ player: "franko-miocic", score: 6 }, { player: "zeljko-bilic", score: 5 }] },
       { start: "17:00", end: "17:20", phase: "zavrsnica", stage: "1/4", bracket: "glavni", format: "6", sides: [{ player: "antonio-jonjic", score: 6 }, { player: "borna-katavic", score: 4 }] },
-      { start: "17:20", end: "17:40", phase: "zavrsnica", stage: "1/2", bracket: "utjesni", format: "6", sides: [{ player: "ivan-tomic" }, { player: "marko-martinovic" }] },
+      { start: "17:20", end: "17:40", phase: "zavrsnica", stage: "1/2", bracket: "utjesni", format: "6", sides: [{ player: "ivan-tomic", score: 6 }, { player: "marko-martinovic", score: 5 }] },
       { start: "17:40", end: "18:00", phase: "zavrsnica", stage: "1/2", bracket: "utjesni", format: "6", sides: [{ player: "vanja-lopusinsky" }, { player: "mateo-gugic" }] },
       { start: "18:00", end: "18:30", phase: "zavrsnica", stage: "1/2", bracket: "glavni", format: "2x4", sides: [{ player: "mario-soco" }, { player: "jurica-separovic" }] },
       { start: "18:30", end: "19:00", phase: "zavrsnica", stage: "1/2", bracket: "glavni", format: "2x4", sides: [{ player: "antonio-jonjic" }, { player: "franko-miocic" }] },
